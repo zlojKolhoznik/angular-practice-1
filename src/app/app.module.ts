126d4e675a96157038aca783b58021616e09b83b
Roman Portianko <rportyanko10@gmail.com>
2023-02-11 12:47:01 +0200

    Tasks 1 and 2 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HobbyComponent } from './hobby/hobby.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HobbyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hobby/hobby.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Hobby } from '../hobby';
+
+@Component({
+  selector: 'app-hobby',
+  templateUrl: './hobby.component.html',
+  styleUrls: ['./hobby.component.css']
+})
+export class HobbyComponent {
+  hobby: Hobby = new Hobby('tree shaping', 'It\'s a very cool hobby in which you train living trees and other woody plants into artistic shapes or useful structures. Think about growing your own chair! It might take a while though.');
+}
